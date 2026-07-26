51c3730e0cc6114e1566552f5562d6b101416622
Kelvin Kipkirui <kevinmutaih@icloud.com>
2026-07-26 12:25:55 +0300

    fix: set PyMySQL connect_timeout, read_timeout, and write_timeout to 30s in settings.py for robust proxy connection

--- django_backend/bulllogic/settings.py
+++ django_backend/bulllogic/settings.py
@@ -95,7 +95,13 @@ if DATABASE_URL and dj_database_url:
                 parsed_db['ENGINE'] = 'django.db.backends.mysql'
                 parsed_db['OPTIONS'] = {'unix_socket': socket_path}
             elif os.getenv('K_SERVICE') is None:
-                pass
+                options = parsed_db.get('OPTIONS', {})
+                options.update({
+                    'connect_timeout': 30,
+                    'read_timeout': 30,
+                    'write_timeout': 30,
+                })
+                parsed_db['OPTIONS'] = options
         if parsed_db:
             engine = parsed_db.get('ENGINE', '')
             if 'sql_server' in engine or 'mssql' in engine:
